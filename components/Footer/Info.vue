b8f81fce49e35caa68da203b362765b949ba874f
Michał 74172 <michalszwed123@gmail.com>
2024-03-03 22:19:33 +0100

    add footer with components

---
+++ components/Footer/Info.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="level">
+        <!-- Left side -->
+        <div class="level-left">
+          <div class="level-item" style="font-size: 0.75rem;">
+            © 2024 Uniwersytet WSB Merito. Wszelkie prawa zastrzeżone.
+          </div>
+        </div>
+        <!-- Right side -->
+        <div class="level-right">
+          <div class="level-item" style="font-size: 0.75rem;">
+            Proudly made by WSB
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  // Your script content (if any) will go here
+}
+</script>
+
+<style scoped>
+.footer {
+  background-color: #111;
+  padding: 1rem;
+  color: white;
+}
+.level-item {
+  font-size: 0.75rem; /* Smaller font size */
+}
+</style>
