1386cc5dc216b3d0006685b0de7fd21a1121598b
Michał 74172 <michalszwed123@gmail.com>
2024-03-03 22:26:01 +0100

    remove footer from info

--- components/Footer/Info.vue
+++ components/Footer/Info.vue
@@ -1,14 +1,11 @@
 <template>
-  <footer class="footer">
     <div class="container">
       <div class="level">
-        <!-- Left side -->
         <div class="level-left">
           <div class="level-item" style="font-size: 0.75rem;">
             © 2024 Uniwersytet WSB Merito. Wszelkie prawa zastrzeżone.
           </div>
         </div>
-        <!-- Right side -->
         <div class="level-right">
           <div class="level-item" style="font-size: 0.75rem;">
             Proudly made by WSB
@@ -16,12 +13,10 @@
         </div>
       </div>
     </div>
-  </footer>
 </template>
 
 <script>
 export default {
-  // Your script content (if any) will go here
 }
 </script>
 
@@ -32,6 +27,5 @@ export default {
   color: white;
 }
 .level-item {
-  font-size: 0.75rem; /* Smaller font size */
-}
+  font-size: 0.75rem;
 </style>
