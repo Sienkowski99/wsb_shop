ecd8dcc3e677dd17a93570602b7f62c752d53f76
Seweryn Sergjeiusz 40038 <campery11@gmail.com>
2024-03-08 18:25:43 +0100

    Initiated and filled tiles

---
+++ components/Tile/BaseView.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="tile is-ancestor">
+        <div class="tile is-parent is-4">
+            <article class="tile is-child box">
+                <figure class="image">
+                    <img src="public\clothes1.jpg">
+                </figure>
+                <br />
+                <p class="title is-5">Marynarka Nana</p>
+                <p class="subtitle is-6">Już od: <span class="is-italic is-size-5">549,00 zł</span></p>
+            </article>
+        </div>
+        <div class="tile is-parent is-4">
+            <article class="tile is-child box">
+                <figure class="image">
+                    <img src="public\clothes2.jpg">
+                </figure>
+                <br />
+                <p class="title is-5">Koszula Koral White</p>
+                <p class="subtitle is-6">Już od: <span class="is-italic is-size-5">260,99 zł</span></p>
+            </article>
+        </div>
+        <div class="tile is-parent is-4">
+            <article class="tile is-child box">
+                <figure class="image">
+                    <img src="public\clothes3.jpg">
+                </figure>
+                <br />
+                <p class="title is-5">Spódnica Waja Magenta</p>
+                <p class="subtitle is-6">Już od: <span class="is-italic is-size-5">243,82 zł</span></p>
+            </article>
+        </div>
+    </div>
+    <div class="tile is-ancestor">
+        <div class="tile is-parent is-4">
+            <article class="tile is-child box">
+                <figure class="image">
+                    <img src="public\clothes4.jpg">
+                </figure>
+                <br />
+                <p class="title is-5">Spódnica waja green</p>
+                <p class="subtitle is-6">Już od: <span class="is-italic is-size-5">227,56 zł</span></p>
+            </article>
+        </div>
+        <div class="tile is-parent is-4">
+            <article class="tile is-child box">
+                <figure class="image">
+                    <img src="public\clothes5.jpg">
+                </figure>
+                <br />
+                <p class="title is-5">Koszula Koral black</p>
+                <p class="subtitle is-6">Już od: <span class="is-italic is-size-5">390,00 zł</span></p>
+            </article>
+        </div>
+        <div class="tile is-parent is-4">
+            <article class="tile is-child box">
+                <figure class="image">
+                    <img src="public\clothes6.jpg">
+                </figure>
+                <br />
+                <p class="title is-5">Płaszcz Teddy black</p>
+                <p class="subtitle is-6">Już od: <span class="is-italic is-size-5">569,16 zł</span></p>
+            </article>
+        </div>
+    </div>
+    <div class="tile is-ancestor">
+        <div class="tile is-parent is-4">
+            <article class="tile is-child box">
+                <figure class="image">
+                    <img src="public\clothes7.jpg">
+                </figure>
+                <br />
+                <p class="title is-5">Bluza Leon White</p>
+                <p class="subtitle is-6">Już od: <span class="is-italic is-size-5">130,00 zł</span></p>
+            </article>
+        </div>
+        <div class="tile is-parent is-4">
+            <article class="tile is-child box">
+                <figure class="image">
+                    <img src="public\clothes8.jpg">
+                </figure>
+                <br />
+                <p class="title is-5">Bluza Leon Black</p>
+                <p class="subtitle is-6">Już od: <span class="is-italic is-size-5">146,00 zł</span></p>
+            </article>
+        </div>
+        <div class="tile is-parent is-4">
+            <article class="tile is-child box">
+                <figure class="image">
+                    <img src="public\clothes9.jpg">
+                </figure>
+                <br />
+                <p class="title is-5">Bluzka Zorra Avocado</p>
+                <p class="subtitle is-6">Już od: <span class="is-italic is-size-5">121,87 zł</span></p>
+            </article>
+        </div>
+    </div>
+</template>
