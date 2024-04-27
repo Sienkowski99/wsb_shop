5bdb179c6a32c6f6ecf4355fa7cc40641216aaf5
Damian 73325 <gdx163831@student.wsb.gda.pl>
2024-04-27 13:10:41 +0200

    Added modal

--- components/Footer/Links.vue
+++ components/Footer/Links.vue
@@ -11,7 +11,7 @@
           <li><a href="#">Reklamacje</a></li>
           <li><a href="#">Zwroty, wymiana</a></li>
           <li><a href="#">Kontakt</a></li>
-          <li><a href="#">Testowy link</a></li>
+          <li><a @click="toggleModal">Zgłoś błąd</a></li>
         </ul>
       </div>
       <div class="column">
@@ -38,11 +38,37 @@
       </div>
     </div>
   </div>
+
+  <div v-if="showModal" class="modal is-active">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Zgłoś błąd</p>
+        <button class="delete" aria-label="close" @click="toggleModal"></button>
+      </header>
+      <section class="modal-card-body">
+        <textarea class="textarea" placeholder="Opisz błąd tutaj..." v-model="issueContent"></textarea>
+      </section>
+      <footer class="modal-card-foot">
+        <button class="button is-success" @click="submitIssue">Submit</button>
+        <button class="button" @click="toggleModal">Cancel</button>
+      </footer>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-}
+<script setup>
+  const showModal = ref(false);
+  const issueContent = ref('');
+  const toggleModal = () => {
+    showModal.value = !showModal.value;
+    if (!showModal.value) issueContent.value = '';
+  }
+  const submitIssue = () => {
+    console.log('Issue submitted: ', issueContent.value);
+    toggleModal();
+  }
+
 </script>
 
 <style scoped>
